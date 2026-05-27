88fef57238898bd55f34b55bc019e2ac26ade7bd
juanalz <juan.f1894@gmail.com>
2026-05-27 14:40:34 -0500

    fix: update file querytransactiondto (userid is added)

--- src/transactions/application/dto/query-transaction.dto.ts
+++ src/transactions/application/dto/query-transaction.dto.ts
@@ -1,4 +1,4 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiPropertyOptional } from "@nestjs/swagger";
 import {
   IsOptional,
   IsEnum,
@@ -6,39 +6,67 @@ import {
   IsUUID,
   Min,
   IsInt,
-} from 'class-validator';
-import { Type } from 'class-transformer';
-import { TransactionType } from '../../domain/entities/transaction.entity';
+} from "class-validator";
+import { Type } from "class-transformer";
+import { TransactionType } from "../../domain/entities/transaction.entity";
 
 export class QueryTransactionDto {
-  @ApiPropertyOptional({ enum: TransactionType, description: 'Filtrar por tipo' })
+  @ApiPropertyOptional({
+    enum: TransactionType,
+    description: "Filtrar por tipo",
+  })
   @IsOptional()
   @IsEnum(TransactionType)
   type?: TransactionType;
 
-  @ApiPropertyOptional({ example: 'fb160441-660f-4e4d-af0b-b65d1a368b6f', description: 'Filtrar por ID de categoría (UUID)' })
+  @ApiPropertyOptional({
+    example: "fb160441-660f-4e4d-af0b-b65d1a368b6f",
+    description: "Filtrar por ID de categoría (UUID)",
+  })
   @IsOptional()
   @IsUUID()
   categoryId?: string;
 
-  @ApiPropertyOptional({ example: '2024-01-01', description: 'Fecha inicial (inclusive)' })
+  @ApiPropertyOptional({
+    example: "fb160441-660f-4e4d-af0b-b65d1a368b6f",
+    description: "ID del usuario",
+  })
+  @IsOptional()
+  @IsUUID()
+  userId?: string;
+
+  @ApiPropertyOptional({
+    example: "2024-01-01",
+    description: "Fecha inicial (inclusive)",
+  })
   @IsOptional()
   @IsDateString()
   from?: string;
 
-  @ApiPropertyOptional({ example: '2024-12-31', description: 'Fecha final (inclusive)' })
+  @ApiPropertyOptional({
+    example: "2024-12-31",
+    description: "Fecha final (inclusive)",
+  })
   @IsOptional()
   @IsDateString()
   to?: string;
 
-  @ApiPropertyOptional({ example: 1, description: 'Número de página (comienza en 1)', default: 1 })
+  @ApiPropertyOptional({
+    example: 1,
+    description: "Número de página (comienza en 1)",
+    default: 1,
+  })
   @IsOptional()
   @IsInt()
   @Min(1)
   @Type(() => Number)
   page?: number = 1;
 
-  @ApiPropertyOptional({ example: 20, description: 'Resultados por página', default: 20 })
+  @ApiPropertyOptional({
+    example: 20,
+    description: "Resultados por página",
+    default: 20,
+  })
   @IsOptional()
   @IsInt()
   @Min(1)
